60eca3469687c14f43d843173557ee274c4136a6
Vasily Mrachkovsky <v.mrachkovsky@pravo.ru>
2017-12-05 14:29:24 +0400

    [feature-tree] publish

--- src/component/contents/DraftEditorBlocks.react.js
+++ src/component/contents/DraftEditorBlocks.react.js
@@ -178,9 +178,9 @@ class DraftEditorBlocks extends React.Component {
     const dataBlocks = dataContents ? null : true;
 
     return (
-      <>
-        {blocks}
-      </>
+      // data-contents will be true for the root level block otherwise
+      // it will just be a block container
+      <div data-contents={dataContents} data-blocks={dataBlocks} >{blocks}</div>
     );
   }
 }
